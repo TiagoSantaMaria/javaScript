d858e89224a15fd8eb263b72956d9f7067c369f6
Tiago Santa Maria <tiagosantamaria01@gmail.com>
2022-06-04 20:21:29 -0300

    Entrega SegundoDesafio

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
-alert("Buenos dias --- Carga y Descarga de piezas de Ferreteria")
-let nombreObjeto = prompt("Ingrese el nombre para la pieza"); 
-while (nombreObjeto === ""){
-    nombreObjeto = prompt("Por favor ingrese un nombre para la pieza");
+let nombrePieza;
+let piezasDisponibles;
+let piezasUsadas;
+
+const cargaPieza = () => {
+    alert("Buenos dias --- Carga y Descarga de piezas de Ferreteria");
+    nombrePieza = prompt("Ingrese el nombre para la pieza"); 
+    while (nombrePieza === ""){
+    nombrePieza = prompt("Por favor ingrese un nombre para la pieza");
+    }
+    do{
+        piezasDisponibles = Number(prompt(`Cantidad de piezas disponibles de ${nombrePieza}`));
+        console.log(isNaN(piezasDisponibles));
+    }while (isNaN(piezasDisponibles) !== false);
 }
-let piezasDisponibles = Number(prompt(`Cantidad de piezas disponibles de ${nombreObjeto}`));
-while (piezasDisponibles !== 0){
-    let piezasUsadas = Number(prompt("Cantidad de piezas usadas?"))
-    if (piezasUsadas<=piezasDisponibles){
-        piezasDisponibles = piezasDisponibles - piezasUsadas;
-        alert(`Quedan ${piezasDisponibles} piezas de ${nombreObjeto}`);
-    }else if (piezasUsadas>piezasDisponibles){
-        alert(`No se pudo realizar la accion, solo quedan ${piezasDisponibles} piezas`);
+
+const usoPieza = () => {
+    while (piezasDisponibles !== 0){
+        do{
+            piezasUsadas = Number(prompt("Cantidad de piezas usadas?"));
+        }while (isNaN(piezasUsadas) !== false);
+        if (piezasUsadas<=piezasDisponibles){
+            piezasDisponibles = piezasDisponibles - piezasUsadas;
+            alert(`Quedan ${piezasDisponibles} piezas de ${nombrePieza}`);
+        }else if (piezasUsadas>piezasDisponibles){
+            alert(`No se pudo realizar la accion, solo quedan ${piezasDisponibles} piezas`);
+        }
     }
 }
+
+const ferreteria = () => {
+    cargaPieza();
+    usoPieza();
+};
+
+ferreteria();
