899499c15f668433f760dca89edfbcc1861e712a
Tiago Santa Maria <tiagosantamaria01@gmail.com>
2022-06-20 00:42:32 -0300

    controlCantReservasMaximasPorDia

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ class Restaurante{
         this.direccion = `Siria 381`;
         this.usuario = `session`;
         this.clave = `123`;
+        this.reservasMaxPorDia = 10;
         this.reservas = [];
     }
     mostrarMenu(){
@@ -36,7 +37,12 @@ class Restaurante{
     cargarDatosReserva(){
         let res = new Reserva();
         res.cargaDatos();
-        this.reservas.push(res);
+        if (this.controlMaxReservas(res.diaReserva, res.cantPersonas) === 'true'){;
+            this.reservas.push(res);
+            alert(`Reserva Registrada`)
+        }else{
+            alert(`La reserva no pudo ser registrada ya que se alcanzo el limite disponible.`)
+        }
     }
     confirmarReserva(){
         let nombreReserva = prompt(`Ingrese el nombre del dueño de la reserva:`);
@@ -46,21 +52,25 @@ class Restaurante{
     }
     mostrarProximasReservas(){
         const reservasProximas = this.reservas.filter(reservasProx => reservasProx.reservaPendiente === 'true');
-        reservasProximas.sort(() => { //Aca quiero ordenar para q me muestre primero las fechas proximas pero no pude :(
-            if (reservasProximas.diaReserva > reservasProximas.diaReserv) {
-                return 1;
-                }
-            if (reservasProximas.diaReserva < reservasProximas.diaReserva) {
-                return -1;
-                }
-            return 0;
-        })
+        reservasProximas.sort((a, b) => a.diaReserva - b.diaReserva);
+        console.log(reservasProximas);
         reservasProximas.forEach( reservaProx => {
             console.log(`Prox Reserva:
             Nombre: ${reservaProx.nombreResponsable} 
             CantPersonas: ${reservaProx.cantPersonas}
             Dia Reserva: ${reservaProx.diaReserva}`);
         })
+    }
+    controlMaxReservas(fechaCorrienteReserva,cantPersonas){
+        let capacidadAcumulada = cantPersonas;
+        this.reservas.forEach( reserva => {
+            if (reserva.diaReserva.getTime() === fechaCorrienteReserva.getTime()){
+                capacidadAcumulada =  capacidadAcumulada + reserva.cantPersonas;
+            }
+        })
+        if (capacidadAcumulada <= this.reservasMaxPorDia){
+            return 'true';
+        }
     }
 }
 class Reserva{
